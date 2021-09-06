18b11fc559d32b5fc6f80a3fb477ce011f01f244
Vishwajit Rahatal <rahatalvishwajit@gmail.com>
2021-09-06 13:44:52 +0530

    Core Java Initial Code

---
+++ Core Java/src/main/java/corejava/rating/CompositeValue1.java
@@ -0,0 +1,15 @@
+package corejava.rating;
+
+public class CompositeValue1 {
+
+	int marks;
+	int size;
+	
+	public CompositeValue1(int marks, int size) {
+
+		this.marks = marks;
+		this.size = size;
+	}
+	
+	
+}
---
+++ Core Java/src/main/java/corejava/rating/CompositeValue2.java
@@ -0,0 +1,18 @@
+package corejava.rating;
+
+public class CompositeValue2 {
+
+	float testscore;
+	float labscore;
+	float quizscore;
+	float projectscore;
+	float overallScore;
+	
+	public CompositeValue2(float testscore2, float labscore2, float quizscore2, float projectscore2, float overallScore2) {
+		this.testscore = testscore2;
+		this.labscore = labscore2;
+		this.quizscore = quizscore2;
+		this.projectscore = projectscore2;
+		this.overallScore = overallScore2;
+	}
+}
